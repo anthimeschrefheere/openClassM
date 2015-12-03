968f6e102a6ee54ddd85e2f4975ac12819ee3cae
anthimeschrefheere <anthime.schrefheere@gmail.com>
2015-12-03 22:12:40 +0100

    Update wsgi.py

--- openClassM/openClassM/wsgi.py
+++ openClassM/openClassM/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openclassM.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openClassM.settings")
 
 application = get_wsgi_application()
